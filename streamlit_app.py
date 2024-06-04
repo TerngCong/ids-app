a3df98a74506cd67cd00e8898a6302eda2aee683
TerngCong <terngcong@gmail.com>
2024-06-04 19:57:41 +0800

    bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ def predict_performance(features):
 EmployeeType = st.selectbox("Employee Type", [0, 1, 2], format_func=lambda x: 'Contract' if x == 0 else 'Full Time' if x == 1 else 'Part Time')
 PayZone = st.selectbox("Pay Zone", [0, 1, 2], format_func=lambda x: 'Zone A' if x == 0 else 'Zone B' if x == 1 else 'Zone C')
 DepartmentType = st.selectbox("Department Type", [0, 1, 2, 3, 4, 5], format_func=lambda x: 'Admin Offices' if x == 0 else 'Executive Office' if x == 1 else 'IT / IS' if x == 2 else 'Production' if x == 3 else 'Sales' if x == 4 else 'Software Engineering')
-GenderCode = st.selectbox("Gender", [0, 1], format_func=lambda x: 'Female' if x == 0 else 'Male'
+GenderCode = st.selectbox("Gender", [0, 1], format_func=lambda x: 'Female' if x == 0 else 'Male')
 RaceDesc = st.selectbox("Race", [0, 1, 2, 3, 4], format_func=lambda x: 'Asian' if x == 0 else 'Black' if x == 1 else 'Hispanic' if x == 2 else 'Other' if x == 3 else 'White')
 MaritalDesc = st.selectbox("Marital Status", [0, 1, 2, 3], format_func=lambda x: 'Divorced' if x == 0 else 'Married' if x == 1 else 'Single' if x == 2 else 'Widowed')
 CurrentEmployeeRating = st.slider("Current Employee Rating", 0, 5, 3)
